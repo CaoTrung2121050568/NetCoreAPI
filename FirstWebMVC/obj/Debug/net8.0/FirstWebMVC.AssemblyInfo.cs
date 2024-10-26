9cd8235c9d11236cb563d28998055f50835c0f77
“CaoTrung2121050568” <caovantrung231103@gmail.com>
2024-10-26 14:09:14 +0700

    ...

--- FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec5e9c5b535ba5a139ef9ef56a2e98acc56112a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5afbb986e5ee8cacfedff34e97da2ffca261b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
